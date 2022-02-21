c1c6e935d1c6f86866a9620cfe346ae94516da28
reza shakeri <r.shakeri9210@gmail.com>
2022-02-21 22:32:57 +0330

    Update app.module.ts

--- apps/power-plant/src/app/app.module.ts
+++ apps/power-plant/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
