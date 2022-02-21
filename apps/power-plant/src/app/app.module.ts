009f5c593ea34623237804f76fabc0dff85fa374
reza shakeri <r.shakeri9210@gmail.com>
2022-02-21 22:39:16 +0330

    [feat] angular material installed

--- apps/power-plant/src/app/app.module.ts
+++ apps/power-plant/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, CoreModule],
+  imports: [BrowserModule, HttpClientModule, CoreModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
