b6bfa408a467db22f0ff21609ea8583af889b639
reza shakeri <r.shakeri9210@gmail.com>
2022-02-22 13:08:36 +0330

    [feat] wip

--- apps/power-plant/src/app/layout/layout.module.ts
+++ apps/power-plant/src/app/layout/layout.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
 import { LayoutRoutingModule } from './layout.routing';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
-const matModules = [];
+const matModules = [
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTooltipModule,
+  MatToolbarModule,
+  MatListModule,
+];
 
 @NgModule({
   declarations: [LayoutComponent],
---
+++ apps/power-plant/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+const matModules = [];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class SharedModule {}
