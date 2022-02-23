469403bf21cf23a91c46f91d121a3e149d17c1bc
reza shakeri <r.shakeri9210@gmail.com>
2022-02-24 00:50:05 +0330

    [feat] mat-drawer added

--- apps/power-plant/src/app/layout/components/layout/layout.component.ts
+++ apps/power-plant/src/app/layout/components/layout/layout.component.ts
@@ -1,15 +1,19 @@
+import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'power-plant-angular-nest-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
+  isExpanded = true;
+  constructor() {}
   ngOnInit(): void {
+    this.isExpanded = localStorage.getItem('FOLDED') == 'true' ? true : false;
   }
 
+  logger() {
+    localStorage.setItem('FOLDED', this.isExpanded.toString());
+  }
 }
