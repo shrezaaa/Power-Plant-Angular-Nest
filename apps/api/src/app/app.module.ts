06575a3ad68acbfd3f8041b2b77a89db12ff5590
reza shakeri <r.shakeri9210@gmail.com>
2022-03-09 11:56:45 +0330

    [feat] mssql pure connection added

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MssqlModule } from './mssql/mssql.module';
 
 @Module({
-  imports: [],
+  imports: [MssqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
