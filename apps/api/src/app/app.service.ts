7428acfb986d4e6337375b36b132384ff86c01a1
reza shakeri <r.shakeri9210@gmail.com>
2022-03-23 22:25:59 +0430

    Update app.service.ts

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { InjectConnection } from '@nestjs/typeorm';
-import { Message } from '@power-plant-angular-nest/api-interfaces';
-import { Connection } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  constructor(@InjectConnection() private readonly connection: Connection) {}
+  constructor() {}
 
   getData(): any {
-    return 'hi'
-    // return this.dbService.executeQuery('select * from SolarData.dbo.alarms')
     return { message: 'Welcome to api!' };
   }
 }
