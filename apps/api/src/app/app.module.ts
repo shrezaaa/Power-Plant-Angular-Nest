4fef7c634b0ce225c54d656478385ebf91e04b47
reza shakeri <r.shakeri9210@gmail.com>
2022-03-23 13:57:47 +0430

    [feat] auth module created

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MssqlModule } from './mssql/mssql.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
