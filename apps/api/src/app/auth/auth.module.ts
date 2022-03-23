df0bc8db777b1d41430f84716a9c11e0421c0497
reza shakeri <r.shakeri9210@gmail.com>
2022-03-23 22:27:45 +0430

    [fix[ orm configuration

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
-  providers: [AuthService]
+  imports: [TypeOrmModule.forFeature([UserRepository])],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+}
