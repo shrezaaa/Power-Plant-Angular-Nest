e16785a23cbdd14067becf97c88391994d70b454
reza shakeri <r.shakeri9210@gmail.com>
2022-03-25 00:16:30 +0430

    [feat] new columns added to table

--- apps/api/src/app/auth/dto/auth-credentials.dto.ts
+++ apps/api/src/app/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+import { IsString, MinLength, MaxLength, Matches, ValidateIf } from 'class-validator';
 
 export class AuthCredentialsDto {
   @IsString()
@@ -13,4 +13,16 @@ export class AuthCredentialsDto {
   //   message: 'Password too weak !',
   // })
   password: string;
+
+  @IsString()
+  @ValidateIf((object, value) => value !== null)
+  firstName!:string|null;
+
+  @IsString()
+  @ValidateIf((object, value) => value !== null)
+  lastName!:string|null;
+
+  @IsString()
+  @ValidateIf((object, value) => value !== null)
+  email!:string|null;
 }
