1855e0beb58aec3ea4f9ff007b4b407c50cf0bc2
reza shakeri <r.shakeri9210@gmail.com>
2022-03-24 13:11:33 +0430

    [feat] register component form added

--- apps/power-plant/src/app/auth/components/register/register.component.ts
+++ apps/power-plant/src/app/auth/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'p-plant-register',
@@ -6,10 +9,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  private loginRef$!: Subscription;
+  hidePass1 = true;
+  hidePass2 = true;
+  forms = new FormGroup({
+    username: new FormControl(null, [Validators.required]),
+    first_name: new FormControl(null, [Validators.required]),
+    last_name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    password2: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  pendding = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    // private userSrv: UserService,
+    // private alertSrv: AlertService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRegister() {
+    if (this.forms.valid) {
+      this.pendding = true;
+      this.forms.disable();
+      // this.loginRef$ = this.userSrv
+      //   .register({ ...this.forms.value, role: 'S', password2: undefined })
+      //   .subscribe(
+      //     (response) => {
+      //       this.alertSrv.showToaster('user created Successfully!', 'SUCCESS');
+      //       this.router.navigate(['/auth/login']);
+      //       this.pendding = false;
+      //       this.forms.enable();
+      //     },
+      //     (error) => {
+      //       this.alertSrv.showToaster(
+      //         'please enter valid information',
+      //         'DANGER'
+      //       );
+      //       this.pendding = false;
+      //       this.forms.enable();
+      //     }
+      //   );
+    }
   }
 
+  onRedirectToLogin() {
+    this.router.navigate(['/auth/login']);
+  }
+
+  onCancelRequest() {
+    if (this.pendding) {
+      this.pendding = false;
+      this.forms.enable();
+      this.loginRef$.unsubscribe();
+    }
+  }
 }
