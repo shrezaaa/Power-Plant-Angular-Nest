a6aa2e023e9cff1d4975d92b368c5ad1d2d8e218
reza shakeri <r.shakeri9210@gmail.com>
2022-03-26 03:02:34 +0430

    Update core.module.ts

--- apps/power-plant/src/app/core/core.module.ts
+++ apps/power-plant/src/app/core/core.module.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AuthGuard } from './guard/auth.guard';
 import { GlobalService } from './services/global.service';
+import { HttpsInterceptor } from './services/http.interceptor';
 import { ToastNotificationsModule } from './toast-notification';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ToastNotificationsModule],
+  imports: [CommonModule, ToastNotificationsModule,ToastrModule.forRoot(),
+  ],
   exports: [],
-  providers: [AuthGuard, GlobalService],
+  providers: [
+    AuthGuard,
+    GlobalService,
+    // ToastrService
+    
+  ],
 })
 export class CoreModule {}
