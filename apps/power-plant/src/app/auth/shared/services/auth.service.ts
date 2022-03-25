11c1105dff93705bd0378dae43ad0b39dd51a146
reza shakeri <r.shakeri9210@gmail.com>
2022-03-26 03:02:48 +0430

    [wip]

--- apps/power-plant/src/app/auth/shared/services/auth.service.ts
+++ apps/power-plant/src/app/auth/shared/services/auth.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { BaseHttp } from '../../../core/services/base-http.service';
 import { GlobalService } from '../../../core/services/global.service';
 import { ApiRequest } from '../../../core/services/request.service';
+// import { ApiRequest as ap2 } from '../../../core/services/base-http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private gs: GlobalService) {}
+  constructor(
+    private gs: GlobalService,
+    private injector: Injector,
+    private api56: BaseHttp
+  ) {}
 
   signUp(model) {
     return ApiRequest('POST', true)
@@ -16,11 +22,16 @@ export class AuthService {
       .call(this.gs);
   }
 
-  login(model){
-    return ApiRequest('POST', true)
-      .controller('auth')
-      .action('signin')
-      .addBodies(model)
-      .call(this.gs);
+  login(model) {
+    // return ApiRequest('POST', true)
+    //   .controller('auth')
+    //   .action('signin')
+    //   .addBodies(model)
+    //   .call(this.gs);
+
+    return this.api56
+      .request('POST', 'auth/signin')
+      .setBodyParams(model)
+      .call();
   }
 }
