236261e29ed875ea28b4b5a53c1f63f21704898c
reza shakeri <r.shakeri9210@gmail.com>
2022-03-29 01:19:25 +0430

    [feat] use base http instead of api request :)

--- apps/power-plant/src/app/auth/shared/services/auth.service.ts
+++ apps/power-plant/src/app/auth/shared/services/auth.service.ts
@@ -1,37 +1,23 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BaseHttp } from '../../../core/services/base-http.service';
-import { GlobalService } from '../../../core/services/global.service';
-import { ApiRequest } from '../../../core/services/request.service';
-// import { ApiRequest as ap2 } from '../../../core/services/base-http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(
-    private gs: GlobalService,
-    private injector: Injector,
-    private api56: BaseHttp
-  ) {}
+  constructor(private baseHttp: BaseHttp) {}
 
   signUp(model) {
-    return ApiRequest('POST', true)
-      .controller('auth')
-      .action('signup')
-      .addBodies(model)
-      .call(this.gs);
+    return this.baseHttp
+      .request('POST', 'auth/signup')
+      .setBodyParams(model)
+      .send();
   }
 
   login(model) {
-    // return ApiRequest('POST', true)
-    //   .controller('auth')
-    //   .action('signin')
-    //   .addBodies(model)
-    //   .call(this.gs);
-
-    return this.api56
+    return this.baseHttp
       .request('POST', 'auth/signin')
       .setBodyParams(model)
-      .call();
+      .send();
   }
 }
