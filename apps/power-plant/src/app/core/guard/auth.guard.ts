c390319dfa3047e61f2a84316afb6a3d301e2684
reza shakeri <r.shakeri9210@gmail.com>
2022-04-01 22:11:35 +0430

    [feat] add auth guard

--- apps/power-plant/src/app/core/guard/auth.guard.ts
+++ apps/power-plant/src/app/core/guard/auth.guard.ts
@@ -1,10 +1,10 @@
-import { Injectable, ErrorHandler } from "@angular/core";
+import { Injectable, ErrorHandler } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   CanActivate,
   Router,
-} from "@angular/router";
+} from '@angular/router';
 import {
   HttpInterceptor,
   HttpRequest,
@@ -12,12 +12,12 @@ import {
   HttpEvent,
   HttpErrorResponse,
   HttpResponse,
-} from "@angular/common/http";
-import { tap } from "rxjs/operators";
-import { Observable } from "rxjs";
+} from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate, HttpInterceptor {
   intercept(
@@ -29,9 +29,8 @@ export class AuthGuard implements CanActivate, HttpInterceptor {
         (event: HttpEvent<any>) => {},
         (err: any) => {
           if (err instanceof HttpErrorResponse) {
-            if (this.router.url !== "/profile/login" && err.status === 401) {
-              localStorage.removeItem("Token");
-              this.router.navigate(["/profile/login"]);
+            if (this.router.url !== '/auth/login' && err.status === 401) {
+              this.router.navigate(['/auth/login']);
             }
             this.errorHandler.handleError(err);
           }
@@ -43,11 +42,11 @@ export class AuthGuard implements CanActivate, HttpInterceptor {
   constructor(private router: Router, public errorHandler: ErrorHandler) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    let Token = localStorage.getItem("Token");
+    let Token = localStorage.getItem('Token');
     if (Token) {
       return true;
     }
-    this.router.navigate(["/profile/login"]);
+    this.router.navigate(['/auth/login']);
     return false;
   }
 }
