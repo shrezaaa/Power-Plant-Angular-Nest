3f6f29e5c26ce6593154d9d0a114d5d6fd61f515
reza shakeri <r.shakeri9210@gmail.com>
2022-04-01 22:12:16 +0430

    [feat] user decoder

--- apps/power-plant/src/app/core/services/user.service.ts
+++ apps/power-plant/src/app/core/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { User } from '../models/user-login.model';
+import { User } from '../models/user.model';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -11,22 +12,21 @@ export class UserService {
   >({});
   constructor() {}
 
-  public set isLoggedIn(value: boolean) {
-    this.isLoggedIn = value;
-  }
-
-  public get isLoggedIn(): boolean {
-    return this.isLoggedIn;
-  }
-
   public setCurrentUser(user: User) {
-    this.isLoggedIn = true;
-    localStorage.setItem('Token', user.token);
+    console.log(user);
     localStorage.setItem('User', JSON.stringify(user));
     this.currentUser$.next(user);
   }
 
   public get User(): Partial<User> {
     return this.currentUser$.value;
+  }
+
+  setUserByToken(encodedToken: string): any {
+    let decodedToken = jwt_decode(encodedToken, { header: false });
+    let user = new User(decodedToken);
+    localStorage.setItem('Token', encodedToken);
+    this.setCurrentUser(user);
+    return user;
   }
 }
