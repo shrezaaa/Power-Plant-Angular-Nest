07e54637ce0ef63e5a2df49c1e67e67c36eddeb5
reza shakeri <r.shakeri9210@gmail.com>
2022-04-10 11:15:04 +0430

    updt

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,14 +1,28 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { Connection, QueryResult, Repository } from 'typeorm';
+import { InjectConnection, InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './auth/user.repository';
 
 @Controller()
 @UseGuards(AuthGuard('jwt'))
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-  
+  constructor(
+    private readonly appService: AppService,
+    @InjectConnection() private readonly connection: Connection
+  ) {}
+
   @Get('hello')
   getData(): any {
     return this.appService.getData();
+  }
+
+  @Get('test')
+  get(): any {
+    return this.connection.query(' select * from SolarData.dbo.alarms').then((result: QueryResult) => {
+      
+      return result ;
+    })
   }
 }
