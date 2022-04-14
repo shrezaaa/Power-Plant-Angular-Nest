96042e9abf456ee917cc1416dc71fd4cdf351070
reza shakeri <r.shakeri9210@gmail.com>
2022-04-14 12:59:35 +0430

    plants routing created

---
+++ apps/power-plant/src/app/plants/plants-routing/plants-routing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'p-plant-plants-routing',
+  templateUrl: './plants-routing.component.html',
+  styleUrls: ['./plants-routing.component.scss']
+})
+export class PlantsRoutingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/power-plant/src/app/plants/plants.module.ts
+++ apps/power-plant/src/app/plants/plants.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlantsRoutingComponent } from './plants-routing/plants-routing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PlantsRoutingComponent
+  ],
   imports: [
     CommonModule
   ]
