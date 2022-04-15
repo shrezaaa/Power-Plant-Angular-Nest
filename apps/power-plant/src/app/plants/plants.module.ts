486b08ff23f4b4b588913ef1e2a456b375675d5b
reza shakeri <r.shakeri9210@gmail.com>
2022-04-15 12:36:37 +0430

    [wip]

--- apps/power-plant/src/app/plants/plants.module.ts
+++ apps/power-plant/src/app/plants/plants.module.ts
@@ -4,9 +4,17 @@ import { PlantsRoutingComponent } from './plants-routing/plants-routing.componen
 import { PlantsListComponent } from './components/plants-list/plants-list.component';
 import { PlantsComponent } from './components/plants/plants.component';
 import { PlantsRoutingModule } from './plants.routing';
+import { PlantsFilterComponent } from './components/plants-filter/plants-filter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
+const matModules =[
+  MatFormFieldModule,
+  MatInputModule
+]
 @NgModule({
-  declarations: [PlantsRoutingComponent, PlantsListComponent, PlantsComponent],
-  imports: [CommonModule, PlantsRoutingModule],
+  declarations: [PlantsRoutingComponent, PlantsListComponent, PlantsComponent, PlantsFilterComponent],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule,matModules, PlantsRoutingModule],
 })
 export class PlantsModule {}
