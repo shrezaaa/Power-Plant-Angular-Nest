46f0bcf5bfa40eee4f97ced1864b6a340faf8e62
reza shakeri <r.shakeri9210@gmail.com>
2022-04-15 12:15:42 +0430

    [feat] add requirements

--- apps/power-plant/src/app/plants/plants.module.ts
+++ apps/power-plant/src/app/plants/plants.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlantsRoutingComponent } from './plants-routing/plants-routing.component';
-
-
+import { PlantsListComponent } from './components/plants-list/plants-list.component';
+import { PlantsComponent } from './components/plants/plants.component';
+import { PlantsRoutingModule } from './plants.routing';
 
 @NgModule({
-  declarations: [
-    PlantsRoutingComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PlantsRoutingComponent, PlantsListComponent, PlantsComponent],
+  imports: [CommonModule, PlantsRoutingModule],
 })
-export class PlantsModule { }
+export class PlantsModule {}
