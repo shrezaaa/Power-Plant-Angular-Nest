fd994579384ced0b8b89266e7a985df395aba019
reza shakeri <r.shakeri9210@gmail.com>
2022-04-17 00:30:43 +0430

    [feat] add 3 item to dashboard

--- apps/power-plant/src/app/dashboard/components/produced-power-chart/produced-power-chart.component.ts
+++ apps/power-plant/src/app/dashboard/components/produced-power-chart/produced-power-chart.component.ts
@@ -33,12 +33,6 @@ export class ProducedPowerChartComponent implements OnInit {
       },
       series: [
         {
-          name: 'Plant 1',
-          data: [
-            1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6, 5.6, 1.2, 10, 12, 9.3, 4,
-          ],
-        },
-        {
           name: 'Plant 2',
           data: [20, 29, 37, 36, 44, 45, 50, 58, 20, 50, 70, 60, 70, 150],
         },
