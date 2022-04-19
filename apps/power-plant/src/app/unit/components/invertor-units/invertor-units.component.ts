cf759418bdf6eaeb0fadbfa9448fe338a9c6b44b
reza shakeri <r.shakeri9210@gmail.com>
2022-04-20 01:44:08 +0430

    [feat] invertor page designed

--- apps/power-plant/src/app/unit/components/invertor-units/invertor-units.component.ts
+++ apps/power-plant/src/app/unit/components/invertor-units/invertor-units.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FloatLabelType } from '@angular/material/form-field';
+import { SelectData } from '../../../shared/types/select-data';
 
 @Component({
   selector: 'p-plant-invertor-units',
   templateUrl: './invertor-units.component.html',
-  styleUrls: ['./invertor-units.component.scss']
+  styleUrls: ['./invertor-units.component.scss'],
 })
 export class InvertorUnitsComponent implements OnInit {
+  selectedUnit: SelectData;
+  units: Array<SelectData> = [
+    { name: 'a', value: 1 },
+    { name: 'ab', value: 1 },
+    { name: 'abc', value: 1 },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- apps/power-plant/src/app/unit/unit.module.ts
+++ apps/power-plant/src/app/unit/unit.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 import { UnitRoutingComponent } from './unit-routing/unit-routing.component';
 import { InvertorUnitsComponent } from './components/invertor-units/invertor-units.component';
 import { UnitRoutingModule } from './unit.routing';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
+const matModules = [MatFormFieldModule, MatInputModule];
 @NgModule({
   declarations: [UnitRoutingComponent, InvertorUnitsComponent],
-  imports: [CommonModule, UnitRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    matModules,
+    UnitRoutingModule,
+    SharedModule
+  ],
 })
 export class UnitModule {}
