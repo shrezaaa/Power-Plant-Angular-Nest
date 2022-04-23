2040cd7f7f9ceff787a97c9c1130823699863ab6
reza shakeri <r.shakeri9210@gmail.com>
2022-04-23 17:37:15 +0430

    [feat] plant map separated from list

--- apps/power-plant/src/app/shared/shared.module.ts
+++ apps/power-plant/src/app/shared/shared.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { ListSelectionComponent } from './components/list-selection/list-selection.component';
 
 const MAT_MODULES = [MatIconModule, MatButtonModule];
 
-const COMPONENTS = [];
+const COMPONENTS = [ListSelectionComponent];
 
 const MODULES = [];
 
 const DIRECTIVES = [];
 @NgModule({
-  declarations: [...COMPONENTS, ...DIRECTIVES],
+  declarations: [...COMPONENTS, ...DIRECTIVES, ListSelectionComponent],
   imports: [...MAT_MODULES, ...MODULES, CommonModule],
   exports: [...MAT_MODULES, ...COMPONENTS, ...DIRECTIVES, ...MODULES],
   providers: [],
