9fe20ee9b3653a04dca301f9d7c761d803b20358
reza shakeri <r.shakeri9210@gmail.com>
2022-04-23 15:59:56 +0430

    [feat] use table in units

--- apps/power-plant/src/app/unit/components/inverter-units/inv-unit-table/inv-unit-table.component.ts
+++ apps/power-plant/src/app/unit/components/inverter-units/inv-unit-table/inv-unit-table.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import {
+  ColDef,
+  ColumnResizedEvent,
+  FirstDataRenderedEvent,
+  GridOptions,
+} from 'ag-grid-community';
 
 @Component({
   selector: 'p-plant-inv-unit-table',
   templateUrl: './inv-unit-table.component.html',
-  styleUrls: ['./inv-unit-table.component.scss']
+  styleUrls: ['./inv-unit-table.component.scss'],
 })
 export class InvUnitTableComponent implements OnInit {
+  @ViewChild('grid') grid: AgGridAngular;
+  gridOptions: GridOptions = {
+    columnDefs: [
+      { field: 'make', sortable: true, filter: true },
+      { field: 'model', sortable: true, filter: true },
+      { field: 'price', sortable: true, filter: true },
+    ],
+    defaultColDef: {
+      resizable: true,
+    },
+    onFirstDataRendered: (params: FirstDataRenderedEvent) => {
+      params.api.sizeColumnsToFit();
+    },
+    onColumnResized: (event: ColumnResizedEvent) => {},
+  };
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
