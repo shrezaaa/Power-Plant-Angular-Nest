92ee012768671a75b6857222dccba28ac47c23fb
reza shakeri <r.shakeri9210@gmail.com>
2022-04-23 17:20:56 +0430

    [refactor] folder structure changed in plants module

---
+++ apps/power-plant/src/app/plants/components/plants-list-view/plants-list-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'p-plant-plants-list-view',
+  templateUrl: './plants-list-view.component.html',
+  styleUrls: ['./plants-list-view.component.scss']
+})
+export class PlantsListViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
