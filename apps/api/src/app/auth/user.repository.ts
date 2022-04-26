878d94bd116e611ad459ad5dfc8cafdacdb1cb9f
reza shakeri <r.shakeri9210@gmail.com>
2022-04-27 00:26:38 +0430

    [feat] (API) add extra data payload to token

--- apps/api/src/app/auth/user.repository.ts
+++ apps/api/src/app/auth/user.repository.ts
@@ -34,14 +34,14 @@ export class UserRepository extends Repository<User> {
   }
 
   async validateUserPassword(
-    authCredentialsDto: AuthCredentialsDto,
-  ): Promise<string> {
+    authCredentialsDto: AuthCredentialsDto
+  ): Promise<any> {
     const { username, password } = authCredentialsDto;
 
     const user = await this.findOne({ username });
 
     if (user && (await user.validatePassword(password))) {
-      return user.username;
+      return user;
     } else {
       return null;
     }
