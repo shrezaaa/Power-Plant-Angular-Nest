e7a71170fba2ed69fe96d68bea9c562ae5813396
reza shakeri <r.shakeri9210@gmail.com>
2022-04-28 19:07:18 +0430

    [feat] (api) add dashboard

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './database/database.env';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, DashboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
