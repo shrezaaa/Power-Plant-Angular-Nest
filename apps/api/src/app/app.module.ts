ca3244bcaf913048c5b23c0579b0ab98328365db
reza shakeri <r.shakeri9210@gmail.com>
2022-05-01 23:36:51 +0430

    add unit api

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './database/database.env';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { UnitModule } from './unit/unit.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, DashboardModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, DashboardModule, UnitModule],
   controllers: [AppController],
   providers: [AppService],
 })
