9634ecb235e9473576371c594626c49ca7a638c8
reza shakeri <r.shakeri9210@gmail.com>
2022-05-02 23:16:15 +0430

    [feat] create new units page

---
+++ apps/power-plant/src/app/app-modules/unit/components/units-page/units-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'p-plant-units-page',
+  templateUrl: './units-page.component.html',
+  styleUrls: ['./units-page.component.scss']
+})
+export class UnitsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/power-plant/src/app/app-modules/unit/components/units-page/units-selection/units-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'p-plant-units-selection',
+  templateUrl: './units-selection.component.html',
+  styleUrls: ['./units-selection.component.scss']
+})
+export class UnitsSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
