858b7e7010c58ffc45b1a3bb7901e43c9449b21f
reza shakeri <r.shakeri9210@gmail.com>
2022-05-02 23:38:30 +0430

    [fix] change list selection component

--- apps/power-plant/src/app/shared/components/list-selection/list-selection.component.ts
+++ apps/power-plant/src/app/shared/components/list-selection/list-selection.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SelectData } from '../../types/select-data';
 
 @Component({
@@ -8,8 +8,16 @@ import { SelectData } from '../../types/select-data';
 })
 export class ListSelectionComponent implements OnInit {
   @Input('data') data: Array<SelectData> = [];
+  // @Input('valueProp') valueProp: string;
+  @Input('labelProp') labelProp: string;
   @Input('selected') selectedObject: SelectData;
+  @Output('selectedChange') selectedChange = new EventEmitter<any>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelectItem(item) {
+    this.selectedObject = item;
+    this.selectedChange.emit(item);
+  }
 }
