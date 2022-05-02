84773a8e0626ff1a55d67611ed73ec4b33b9f6c4
reza shakeri <r.shakeri9210@gmail.com>
2022-05-02 23:37:25 +0430

    [feat] units layout

--- apps/power-plant/src/app/app-modules/unit/components/units-page/units-page.component.ts
+++ apps/power-plant/src/app/app-modules/unit/components/units-page/units-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectData } from 'apps/power-plant/src/app/shared/types/select-data';
 
 @Component({
   selector: 'p-plant-units-page',
   templateUrl: './units-page.component.html',
-  styleUrls: ['./units-page.component.scss']
+  styleUrls: ['./units-page.component.scss'],
 })
 export class UnitsPageComponent implements OnInit {
+  units: Array<SelectData> = [
+    { name: 'Inverter a', value: 1 },
+    { name: 'Inverter ab', value: 1 },
+    { name: 'Inverter abc', value: 1 },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelectUnit(event) {
+    console.log(event);
   }
-
 }
