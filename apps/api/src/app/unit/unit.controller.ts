29eae126b401d83fa0f54d9dd2647e0b4ef8556d
reza shakeri <r.shakeri9210@gmail.com>
2022-05-03 13:59:51 +0430

    [feat] (API) unit/search added

--- apps/api/src/app/unit/unit.controller.ts
+++ apps/api/src/app/unit/unit.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UnitService } from './unit.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('unit')
-export class UnitController {}
+export class UnitController {
+  constructor(private readonly unitService: UnitService) {}
+
+  @Get('/search')
+  async getUnits(@Query() params: any): Promise<void> {
+    return this.unitService.getUnits(params);
+  }
+}
