23c6343f710b1068ce4bd5237b794f621e494bb0
reza shakeri <r.shakeri9210@gmail.com>
2022-05-03 21:22:25 +0430

    [feat] unit yield chart updated to support real data

--- apps/power-plant/src/app/app-modules/unit/components/inverter-units/unit-yield-chart/unit-yield-chart.component.ts
+++ apps/power-plant/src/app/app-modules/unit/components/inverter-units/unit-yield-chart/unit-yield-chart.component.ts
@@ -1,121 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ECharts, EChartsOption } from 'echarts';
+import { PowerChart } from '../../../shared/models/power-chart';
 
 @Component({
   selector: 'p-plant-unit-yield-chart',
   templateUrl: './unit-yield-chart.component.html',
   styleUrls: ['./unit-yield-chart.component.scss'],
 })
-export class UnitYieldChartComponent implements OnInit {
-  colors = ['#2196F3'];
-
+export class UnitYieldChartComponent implements OnInit, OnChanges {
+  @Input('data') data: PowerChart;
+  @Output('chartInstanceChange') chartInstanceChange =
+    new EventEmitter<ECharts>();
   chartInstance: ECharts;
-
-  chartOption: EChartsOption = {
-    color: this.colors,
-    title: {
-      text: 'Today Yield (kWh)',
-    },
-    tooltip: {
-      trigger: 'axis',
-      valueFormatter: (value) => value + ' kWh',
-      axisPointer: {
-        type: 'cross',
-      },
-    },
-    // legend: {
-    //   data: ['Today Yield'],
-    // },
-    xAxis: {
-      type: 'category',
-      axisTick: {
-        alignWithLabel: true,
-      },
-      data: [
-        '00:00',
-        '00:30',
-        '01:00',
-        '01:30',
-        '02:00',
-        '02:30',
-        '03:00',
-        '03:30',
-        '04:00',
-        '04:30',
-        '05:00',
-        '05:30',
-        '06:00',
-        '06:30',
-        '07:00',
-        '07:30',
-        '08:00',
-        '08:30',
-        '09:00',
-        '09:30',
-        '10:00',
-        '10:30',
-        '11:00',
-        '11:30',
-        '12:00',
-        '12:30',
-        '13:00',
-        '13:30',
-        '14:00',
-        '14:30',
-        '15:00',
-        '15:30',
-        '16:00',
-        '16:30',
-        '17:00',
-        '17:30',
-        '18:00',
-        '18:30',
-        '19:00',
-        '19:30',
-        '20:00',
-        '20:30',
-        '21:00',
-        '21:30',
-        '22:00',
-        '22:30',
-        '23:00',
-        '23:30',
-        '24:00',
-        '24:30',
-      ],
-    },
-    yAxis: {
-      type: 'value',
-      // name: 'Today Yield',
-      position: 'left',
-      alignTicks: true,
-      offset: 0,
-      axisLine: {
-        show: true,
-        lineStyle: {
-          color: this.colors[0],
-        },
-      },
-      axisLabel: {
-        formatter: '{value} kWh',
-      },
-    },
-
-    series: [
-      {
-        name: 'Today Yield',
-        type: 'bar',
-        // yAxisIndex: 0,
-        data: [null, null, null, null, 0, 1, 1.2, 2.2, 4.0, 4.1, 4.1, 5.4],
-      },
-    ],
-  };
+  chartOption: EChartsOption = {};
+  colors = ['#2196F3'];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.data && changes.data.currentValue) {
+      this.initChartOptions(this.data.categories, this.data.dayPowerSeries);
+      this.chartInstance.hideLoading();
+    }
+  }
+
+  ngOnInit(): void {
+    this.initChartOptions();
+  }
 
   onChartInit($event: ECharts) {
     this.chartInstance = $event;
+    this.chartInstanceChange.emit($event);
+    if (!this.data) this.chartInstance.showLoading();
+  }
+
+  initChartOptions(categories = [], series = []) {
+    this.chartOption = {
+      color: this.colors,
+      title: {
+        text: 'Today Yield (kWh)',
+      },
+      tooltip: {
+        trigger: 'axis',
+        valueFormatter: (value) => value + ' kWh',
+        axisPointer: {
+          type: 'cross',
+        },
+      },
+      // legend: {
+      //   data: ['Today Yield'],
+      // },
+      xAxis: {
+        type: 'category',
+        axisTick: {
+          alignWithLabel: true,
+        },
+        data: categories,
+      },
+      yAxis: {
+        type: 'value',
+        // name: 'Today Yield',
+        position: 'left',
+        alignTicks: true,
+        offset: 0,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: this.colors[0],
+          },
+        },
+        axisLabel: {
+          formatter: '{value} kWh',
+        },
+      },
+
+      series: [
+        {
+          name: 'Today Yield',
+          type: 'bar',
+          // yAxisIndex: 0,
+          data: series,
+        },
+      ],
+    };
   }
 }
