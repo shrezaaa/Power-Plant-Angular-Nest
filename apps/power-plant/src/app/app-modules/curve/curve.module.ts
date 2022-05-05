1323c8e3661f44a81ef3625a585912a07db5b99e
reza shakeri <r.shakeri9210@gmail.com>
2022-05-05 23:47:04 +0430

    [feat] create cure-page

--- apps/power-plant/src/app/app-modules/curve/curve.module.ts
+++ apps/power-plant/src/app/app-modules/curve/curve.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurveRoutingComponent } from './curve-routing/curve-routing.component';
 import { CurveRoutingModule } from './curve.routing';
+import { CurePageComponent } from './components/cure-page/cure-page.component';
 
 @NgModule({
-  declarations: [CurveRoutingComponent],
+  declarations: [CurveRoutingComponent, CurePageComponent],
   imports: [CommonModule, CurveRoutingModule],
 })
 export class CurveModule {}
