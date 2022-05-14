3ffaad97ffb57bdf8c06ff3a8ef780c9ca4df252
reza shakeri <r.shakeri9210@gmail.com>
2022-05-14 12:08:57 +0430

    [feat] plants search added

--- apps/api/src/app/plants/plants.controller.ts
+++ apps/api/src/app/plants/plants.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PlantsService } from './plants.service';
 
 @Controller('plants')
-export class PlantsController {}
+export class PlantsController {
+  constructor(private readonly plantsService: PlantsService) {}
+
+  @Get('search')
+  async getPlants() {
+    return this.plantsService.getPlants();
+  }
+}
