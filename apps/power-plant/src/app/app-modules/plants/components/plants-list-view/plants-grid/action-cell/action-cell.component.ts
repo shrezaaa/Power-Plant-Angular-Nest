feef8bab5f2ad216f9f82355c3a9aecbef9f8726
reza shakeri <r.shakeri9210@gmail.com>
2022-05-31 01:03:59 +0430

    [feat] add route to plant map from grid

--- apps/power-plant/src/app/app-modules/plants/components/plants-list-view/plants-grid/action-cell/action-cell.component.ts
+++ apps/power-plant/src/app/app-modules/plants/components/plants-list-view/plants-grid/action-cell/action-cell.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { ICellRendererParams } from 'ag-grid-community';
+import { PlantModel } from '../../../../shared/models/plant.model';
 
 @Component({
   selector: 'p-plant-action-cell',
   templateUrl: './action-cell.component.html',
-  styleUrls: ['./action-cell.component.scss']
+  styleUrls: ['./action-cell.component.scss'],
 })
 export class ActionCellComponent implements OnInit, ICellRendererAngularComp {
   params: ICellRendererParams;
-  field:string
-  rowData:any
-  cellData:any
-  constructor() {}
+  field: string;
+  rowData: PlantModel;
+  cellData: any;
+  constructor(private readonly router: Router) {}
 
   ngOnInit(): void {}
 
   agInit(params: ICellRendererParams): void {
     console.log(params);
-    this.field=params.colDef.field
-    this.rowData=params.data
-    this.cellData=params.data[this.field]
+    this.field = params.colDef.field;
+    this.rowData = params.data;
+    this.cellData = params.data[this.field];
     this.params = params;
   }
 
   refresh(params: ICellRendererParams) {
     return true;
+  }
+
+  openPlantMap(PlantID: number) {
+    this.router.navigate([`plants/map/${PlantID}`]);
   }
 }
--- apps/power-plant/src/app/app-modules/plants/plants.routing.ts
+++ apps/power-plant/src/app/app-modules/plants/plants.routing.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
       {
         path: 'map',
         component: PlantsMapViewComponent,
+      }, 
+      {
+        path: 'map/:id',
+        component: PlantsMapViewComponent,
       },
     ],
   },
