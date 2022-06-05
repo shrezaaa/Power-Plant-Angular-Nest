2fd681b7c2fcd8dd92ab7656f7bb5f0495dbbe8f
reza shakeri <r.shakeri9210@gmail.com>
2022-06-06 01:21:54 +0430

    [feat] create curve requirements

--- apps/power-plant/src/app/app-modules/curve/curve.module.ts
+++ apps/power-plant/src/app/app-modules/curve/curve.module.ts
@@ -8,11 +8,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { CureDynamicChartComponent } from './components/cure-dynamic-chart/cure-dynamic-chart.component';
 
 const matModules = [MatFormFieldModule, MatInputModule, MatSelectModule];
 
 @NgModule({
-  declarations: [CurveRoutingComponent, CurePageComponent],
+  declarations: [CurveRoutingComponent, CurePageComponent, CureDynamicChartComponent],
   imports: [
     CommonModule,
     matModules,
