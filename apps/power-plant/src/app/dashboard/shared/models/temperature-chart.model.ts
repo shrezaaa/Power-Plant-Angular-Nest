100aee71047854761112c13e32f3ab4344644870
reza shakeri <r.shakeri9210@gmail.com>
2022-06-20 03:04:59 +0430

    [feat] change temperature chart

--- apps/power-plant/src/app/dashboard/shared/models/temperature-chart.model.ts
+++ apps/power-plant/src/app/dashboard/shared/models/temperature-chart.model.ts
@@ -1,11 +1,47 @@
 export class TemperatureChart {
   categories: Array<string> = [];
   currentTemperature: Array<any> = [];
+  series = new Map<number, Array<any>>();
 
   constructor(data) {
-    data.forEach((element) => {
-        this.categories.push(element.Time);
-        this.currentTemperature.push(element.CurrentValue);
+    data.forEach((element, index) => {
+      this.series.forEach((value, key) => {
+        if (key == element.DeviceId) {
+          // console.log(value);
+          // debugger
+          if (
+            value[value.length - 1] == undefined ||
+            Math.abs(+element.CurrentValue - +value[value.length - 1]) < 5
+          ) {
+            value.push(element.CurrentValue);
+          } else {
+            value.push(null)
+          }
+        } else if (value.length < this.categories.length) {
+          value.push(value[value.length - 1]);
+        }
+        // this.series.set(key,value)
       });
+
+      if (!this.series.has(element.DeviceId)) {
+        let temp = [];
+        this.categories.forEach(() => {
+          temp.push(null);
+        });
+
+        this.series.set(element.DeviceId, temp);
+      }
+
+      if (data[index - 1]?.Time != element.Time) {
+        // console.log(this.series.has(element.DeviceId));
+
+        this.categories.push(element.Time);
+
+        // console.log(this.series.get(4))
+
+        // console.log(element.Time);
+      }
+      // this.currentTemperature.push(element.CurrentValue);
+    });
   }
 }
