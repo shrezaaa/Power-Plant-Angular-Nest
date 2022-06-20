84ebb1ab3d81612f3d1902b4773b4c1c4e2c0c45
reza shakeri <r.shakeri9210@gmail.com>
2022-06-20 12:32:26 +0430

    Update yield-trend.model.ts

--- apps/power-plant/src/app/dashboard/shared/models/yield-trend.model.ts
+++ apps/power-plant/src/app/dashboard/shared/models/yield-trend.model.ts
@@ -19,24 +19,24 @@ export class YieldTrendChart {
             +element.CurrentPower < 60000
           ) {
             //Remove Hazards
-            this.currentPowerSeries.push(element.CurrentPower);
+            this.currentPowerSeries.push(+element.CurrentPower / 1000);
           } else {
             this.currentPowerSeries.push(null);
           }
           this.categories.push(element.Time);
-          this.todayYieldSeries.push(element.DayPower);
+          this.todayYieldSeries.push(+element.DayPower / 1000);
         });
         break;
       case 2:
         data.forEach((element) => {
           this.categories.push(datePipe.transform(element.Day, 'MM/dd'));
-          this.todayYieldSeries.push(element.MothPower);
+          this.todayYieldSeries.push(+element.MothPower / 1000);
         });
         break;
       case 3:
         data.forEach((element) => {
           this.categories.push(datePipe.transform(element.Month, 'yyyy/MM'));
-          this.todayYieldSeries.push(element.MonthDayPower);
+          this.todayYieldSeries.push(+element.MonthDayPower / 1000);
         });
         break;
     }
